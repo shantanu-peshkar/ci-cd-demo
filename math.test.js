a0cb6bb620613566d3f8068faa9340ebd2a6f99c
shantanu peshkar <peshkarshantanu@gmail.com>
2025-05-02 12:03:06 +0530

    Initial commit

---
+++ math.test.js
@@ -0,0 +1,9 @@
+const { add, multiply } = require('./math');
+
+test('add 2 + 3 equals 5', () => {
+  expect(add(2, 3)).toBe(5);
+});
+
+test('multiply 4 * 5 equals 20', () => {
+  expect(multiply(4, 5)).toBe(20);
+});
